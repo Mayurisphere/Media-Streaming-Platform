3608c9554bd100fa7cfc3350b867009a1fa06cab
Mayuri <mayurichaudhari2004@gmail.com>
2025-02-12 14:43:26 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -7,4 +7,14 @@ dotenv.config({
 })
 
 
-
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is listening at port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection FAILED !!! ", err);
+    
+})
